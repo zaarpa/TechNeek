0bd8143d07d0f0a24159e8df466e255866104ac3
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2023-07-04 23:26:14 +0600

    signup and login backend

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,12 +1,26 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 const loginUserController = async (req, res) => {
-    res.json({message: req.body.email})
+    const {email, password} = req.body;
+    try{
+        const user = await User.login(email, password);
+        //create a token
+        const token = createToken(user._id);
+        res.status(200).json({email,token})
+    }catch(err){
+        res.status(400).json({message: err.message})
+    }
 }
 const signUpUserController = async (req, res) => {
     const {email, password} = req.body;
     try{
         const user = await User.signup(email, password);
-        res.status(200).json({email,user})
+        //create a token
+        const token = createToken(user._id);
+        res.status(200).json({email,token})
     }catch(err){
         res.status(400).json({message: err.message})
     }
