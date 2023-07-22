6b56ba3554d317d377797ea7a1707176d0f2d1c9
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2023-07-22 22:32:19 +0600

    Protecting api routes

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -1,15 +1,17 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useLogout } from '../../hooks/useLogout';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
 function NavScrollExample() {
-    const {logout} = useLogout();
-    const handleClick = () => {
-        logout();
-    }
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+  const handleClick = () => {
+    logout();
+  };
   return (
     <Navbar expand="lg" bg="primary">
       <Container fluid>
@@ -18,16 +20,23 @@ function NavScrollExample() {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <div>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/signup">Signup</Nav.Link>
-            </div>
-            <div>
-            <Button variant="danger" onClick={handleClick}>Logout</Button>
-            </div>
+            {!user && (
+              <div>
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/signup">Signup</Nav.Link>
+              </div>
+            )}
+
+            {user && (
+              <div>
+                <Button variant="danger" onClick={handleClick}>
+                  Logout
+                </Button>
+              </div>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
---
+++ server/middleware/requireAuth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const requireAuth = async (req, res, next) => {
+    //verify authentication
+    const {authorization} = req.headers
+    if(!authorization){
+        return res.status(401).json({message: 'Authorization token required.'})
+    }
+
+    const token = authorization.split(' ')[1]
+
+    try{
+       const {_id} = jwt.verify(token, process.env.SECRET)
+       req.user = await User.findOne({_id}).select('_id')
+         next()
+    } catch(message){
+        console.log(message)
+        res.status(401).json({message: 'Authorization token invalid.'})
+    }
+}
